5c01858dc56e50ffc4014588b46521402a75db5f
Jos Hirth <github@kaioa.com>
2012-05-05 02:55:22 +0200

    Added a rule for the \9 property hack (targets IE6/7/8 and sometimes 9).

---
+++ src/rules/backslash-9-hack.js
@@ -0,0 +1,26 @@
+/*
+ * Rule: Don't use properties with a underscore prefix.
+ *
+ */
+/*global CSSLint*/
+CSSLint.addRule({
+
+    //rule information
+    id: "backslash-9-hack",
+    name: "Disallow values with a \\9 at the very end",
+    desc: "Checks for the \\9 value hack (targets IE6/7/8 and sometimes 9)",
+    browsers: "All",
+
+    //initialization
+    init: function(parser, reporter){
+        var rule = this,
+			trailingBackslash9 = /\\9$/;
+
+        //check if value ends with "\9"
+        parser.addListener("property", function(event){
+            if (trailingBackslash9.test(event.value.text)){
+                reporter.report("Value with \\9 hack found.", event.property.line, event.property.col, rule);
+            }
+        });
+    }
+});
